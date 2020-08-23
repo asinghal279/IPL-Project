810c04c08badd59eada8ecfbe85eb3fc391e8c92
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-08-23 23:11:51 +0530

    Matches won per team per year function added with the file

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -37,4 +37,36 @@ function matchesPlayed(data){
     fs.writeFileSync(outPath, JSON.stringify(obj), 'utf8', 
     function(err){console.log(err);});
 }
-console.log(matchesPlayed(json));
+
+
+function matchesWon(data)
+{
+    let obj = {}
+    data.forEach(item => {
+        if(obj.hasOwnProperty(item.season))
+        {
+            if(obj[item.season].hasOwnProperty(item.winner))
+            {
+                obj[item.season][item.winner]++;
+            }
+            else
+            {
+                obj[item.season][item.winner]=1;
+            }
+        }
+        else
+        {
+            obj[item.season]={};
+        }
+    })
+    let outPath = '../public/output/matchesWonPerTeamPerYear.json';
+    fs.writeFileSync(outPath, JSON.stringify(obj), 'utf8', 
+    function(err){console.log(err);});
+}
+
+// console.log(json);
+matchesPlayed(json);
+matchesWon(json);
+
+
+
