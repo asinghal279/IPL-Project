f71c0bae8bb7a9463905a5bcdc673a9b7dfa3868
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-08-24 11:54:00 +0530

    Top 10 most economical bowlers in 2015 functionality implemented

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
 
 
 //Reading data from Matches.csv to convert to JSON
@@ -86,7 +87,62 @@ function matchesWon(data)
     function(err){console.log(err);});
 }
 
-console.log(json);
+function top10EconomicalBowlers(matches, deliveries){
+    let matchesin2015 = [];
+    let obj = {};
+    matches.forEach(item => {
+        if(item.season == 2015)
+        {
+            matchesin2015.push(item.id)
+        }
+    })
+    deliveries.forEach(item => {
+        if(matchesin2015.indexOf(item.match_id) > -1)
+        {
+            if(obj.hasOwnProperty(item.bowler))
+            {
+                obj[item.bowler][0] += parseInt(item.total_runs);
+                obj[item.bowler][1]++;
+            }
+            else
+            {
+                obj[item.bowler] = [0,0];
+            }
+        }
+    })
+    let arrayWithEconomies = [];
+    // console.log(obj);
+    for(item in obj){
+        let overs = (obj[item][1])/6;
+        let total_runs = obj[item][0];
+        // console.log(overs);
+        let economy = total_runs/overs;
+        let nest = [item, economy];
+        // console.log(nest);
+        arrayWithEconomies.push(nest);
+    }
+
+    arrayWithEconomies.sort((a,b)=>{
+        return a[1]-b[1];
+    })
+
+    // console.log(arrayWithEconomies.slice(0,10));
+
+    let final_array = [];
+    for(let i = 0;i< 10;i++)
+    {
+        final_array.push(arrayWithEconomies[i][0]);
+    }
+
+    console.log(final_array);
+
+    let outPath = '../public/output/top10EconomicalBowlers.json';
+    fs.writeFileSync(outPath, JSON.stringify(final_array), 'utf8', 
+    function(err){console.log(err);});
+}
+
+top10EconomicalBowlers(json, json_deliveries);
+// console.log(json);
 // matchesPlayed(json);
 // matchesWon(json);
 
