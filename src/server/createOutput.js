1c9b5984227ef7ecc3fab4a7262c364527463972
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-08-25 16:47:25 +0530

    Formatted Code

--- src/server/createOutput.js
+++ src/server/createOutput.js
@@ -1,9 +1,11 @@
 let fs = require("fs");
 
-function createOutput(path, data)
-{
-    fs.writeFileSync(path, data), 'utf8', 
-    function(err){console.log(err);};
+function createOutput(path, data) {
+  fs.writeFileSync(path, data),
+    "utf8",
+    function (err) {
+      console.log(err);
+    };
 }
 
 module.exports = createOutput;
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,15 @@
-let path = require('path');
-let csvtojson = require('./csvToJson');
-let ipl = require('./ipl');
+let path = require("path");
+let csvtojson = require("./csvToJson");
+let ipl = require("./ipl");
 const matchesPlayed = ipl.matchesPlayed;
 const matchesWon = ipl.matchesWon;
-const top10EconomicalBowlers = ipl.top10EconomicalBowlers
+const top10EconomicalBowlers = ipl.top10EconomicalBowlers;
 const extraRuns = ipl.extraRunsPerTeam;
-let createOutput = require('./createOutput');
-
+let createOutput = require("./createOutput");
 
 // Setting the Paths to CSV
-let matchesPath = path.join(__dirname, '../data/matches.csv');
-let deliveriesPath = path.join(__dirname, '../data/deliveries.csv');
+let matchesPath = path.join(__dirname, "../data/matches.csv");
+let deliveriesPath = path.join(__dirname, "../data/deliveries.csv");
 
 //convert to JSON
 let matches_json = csvtojson(matchesPath);
@@ -21,17 +20,28 @@ const year1 = 2015;
 const year2 = 2016;
 
 // Setting the paths to OutputFiles
-let outPath1 = path.join(__dirname,'../public/output/matchPerYear.json');
-let outPath2 = path.join(__dirname,'../public/output/matchesWonPerTeamPerYear.json');
-let outPath3 = path.join(__dirname,`../public/output/top10EconomicalBowlersIn${year1}.json`);
-let outPath4 = path.join(__dirname,`../public/output/extraRunsPerTeamIn${year2}.json`);
+let outPath1 = path.join(__dirname, "../public/output/matchPerYear.json");
+let outPath2 = path.join(
+  __dirname,
+  "../public/output/matchesWonPerTeamPerYear.json"
+);
+let outPath3 = path.join(
+  __dirname,
+  `../public/output/top10EconomicalBowlersIn${year1}.json`
+);
+let outPath4 = path.join(
+  __dirname,
+  `../public/output/extraRunsPerTeamIn${year2}.json`
+);
 
 // Creating Output Files with data
-createOutput(outPath1, JSON.stringify(matchesPlayed(matches_json)))
-createOutput(outPath2, JSON.stringify(matchesWon(matches_json)))
-createOutput(outPath3, JSON.stringify(top10EconomicalBowlers(matches_json,deliveries_json,year1)));
-createOutput(outPath4, JSON.stringify(extraRuns(matches_json,deliveries_json,year2)));
-
-
-
-
+createOutput(outPath1, JSON.stringify(matchesPlayed(matches_json)));
+createOutput(outPath2, JSON.stringify(matchesWon(matches_json)));
+createOutput(
+  outPath3,
+  JSON.stringify(top10EconomicalBowlers(matches_json, deliveries_json, year1))
+);
+createOutput(
+  outPath4,
+  JSON.stringify(extraRuns(matches_json, deliveries_json, year2))
+);
